1fea11d3aeeddba8363621ae2c7196211ed58db8
DAGOUAGA Patrick <patrickdagouaga@gmail.com>
2022-05-15 03:36:18 +0000

    integration des page ook

--- front/urls.py
+++ front/urls.py
@@ -1,6 +1,14 @@
 from django.urls import path
-from .views import index
+from . import views
 
 urlpatterns = [
-    path('',index,name="index"),
+    
+    path('',views.index,name="home"),
+    path('about',views.about,name="about"),
+    path('property',views.property,name="property"),
+    #TODO : FIND THE WEY TO PARSE THE SLUG 
+    path('property-single/<int:property_id>',views.property_single,name="property_single"),
+    path('agent',views.agent,name="agent"),
+    path('agent-single/<int:angent_id>',views.agent_single,name="agent_single"),
+    path('contact',views.contact,name="contact"),
 ]
--- front/views.py
+++ front/views.py
@@ -4,5 +4,25 @@ from django.shortcuts import render
 
 
 def index(request):
+    return render(request,'pages/home.html')
 
-    return render(request,'pages/index.html')
+
+def about(request):
+    return render(request,'pages/about.html')
+
+def property(request):
+    return render(request,'pages/property.html')
+
+def property_single(request,property_id):
+    return render(request,'pages/property_single.html')
+
+
+def agent(request):
+    return render(request,'pages/agent.html')
+
+def agent_single(request,agent_id):
+    return render(request,'pages/agent_single.html')
+
+
+def contact(request):
+    return render(request,'pages/contact.html')
